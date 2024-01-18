e87cd1a2b8add3651389750046d4954bbd02e0d2
Rifqi Pinasthika <v-rifqi.pinasthika@traveloka.com>
2024-01-18 12:36:35 +0700

    Update captcha parameter to path & test malindo

--- Traveloka_DataAutomation_Portal_Malindoariwayssdnbhd/Traveloka_DataAutomation_Captcha_Handler/responseBreaker.js
+++
@@ -1,6 +0,0 @@
-function(element,input){
-    document.getElementsByName("g-recaptcha-response").item(0).value=input;
-    var button = document.getElementById('btnSbmt');
-    button.click();
-    //document.querySelector('form').submit();
-}
